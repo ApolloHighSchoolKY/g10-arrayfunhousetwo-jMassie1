9d1a9f72778037d5b2cbef70ca27dd62cdba1faf
jMassie1 <jaden.massie@stu.daviess.kyschools.us>
2025-01-14 13:52:11 +0000

    hw

--- Lab14b.java
+++ Lab14b.java
@@ -1,4 +1,4 @@
-//© A+ Computer Science  -  www.apluscompsci.com
+//ï¿½ A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
 //Class -
@@ -12,6 +12,8 @@ public class Lab14b
 	{
 		int[] one = {1,2,3,4,5,6,7,8,9,10};
 		int[] two = {1,2,3,9,11,20,30};
+		int[] three = {9,8,7,6,5,4,3,2,0,-2};
+		int[] four = {3,6,9,12,15,18,21,23,19,17,15,13,11,10,9,6,3,2,1,0};
 		//add more test cases
 
 
@@ -19,11 +21,21 @@ public class Lab14b
 		System.out.println("is going Up ?  " + ArrayFunHouseTwo.goingUp(one));
 		System.out.println(Arrays.toString(two));
 		System.out.println("is going Up ?  " + ArrayFunHouseTwo.goingUp(two));
+		System.out.println(Arrays.toString(three));
+		System.out.println("is going Up ?  " + ArrayFunHouseTwo.goingUp(three));
+		System.out.println(Arrays.toString(four));
+		System.out.println("is going Up ?  " + ArrayFunHouseTwo.goingUp(four));
 		//add more test cases
 
 		System.out.println("\n\n"+Arrays.toString(one));
 		System.out.println("is going Down ?  " + ArrayFunHouseTwo.goingDown(one));
 		System.out.println(Arrays.toString(two));
+		System.out.println("is going Down ?  " + ArrayFunHouseTwo.goingDown(two));
+		System.out.println(Arrays.toString(three));
+        System.out.println("is going Down ?  " + ArrayFunHouseTwo.goingDown(three));
+		System.out.println(Arrays.toString(four));
+		System.out.println("is going Down ?  " + ArrayFunHouseTwo.goingDown(four));
+
 		//add more test cases
 
 		System.out.println("\n\n first 3 values greater than 8" + Arrays.toString(ArrayFunHouseTwo.getCountValuesBiggerThanX(two,3,8))  );
